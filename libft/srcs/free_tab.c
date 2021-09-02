369491265ec9a39d6b9407c229eaf92c0605728b
Terry ALLAIRE <tallaire@e1r9p10.42.fr>
2021-09-02 16:03:43 +0200

    save

--- libft/srcs/free_tab.c
+++ libft/srcs/free_tab.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_tab.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tallaire <tallaire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/02 16:02:06 by tallaire          #+#    #+#             */
+/*   Updated: 2021/09/02 16:02:22 by tallaire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./../includes/libft.h"
 
-char    **free_tab(char **tab)
+char	**free_tab(char **tab)
 {
-    int     i;
+	int		i;
 
-    i = 0;
-    while (tab && tab[i])
-    {
-            free(tab[i]);
-            ++i;
-    }
-    free(tab);
-    tab = NULL;
-    return (NULL);
+	i = 0;
+	while (tab && tab[i])
+	{
+		free(tab[i]);
+		++i;
+	}
+	free(tab);
+	tab = NULL;
+	return (NULL);
 }
